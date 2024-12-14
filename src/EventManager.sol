174978e1f286d49d164ad66c632c51c9fec3863c
Aroso Emmanuel <emmanueladedejiaroso@gmail.com>
2024-12-14 13:01:46 +0100

    setup and test

--- src/EventManager.sol
+++ src/EventManager.sol
@@ -2,59 +2,61 @@
 pragma solidity ^0.8.13;
 
 contract EventManager {
+  ////////////////////////////////////////////////////////////////
+  ///                      CONFIG STORAGE                      ///
+  ////////////////////////////////////////////////////////////////
+  /// @dev The address of the World ID Router contract that will be used for verifying proofs
+  IWorldID internal immutable worldId;
+  // address public worldID;
 
-    ////////////////////////////////////////////////////////////////
-    ///                      CONFIG STORAGE                      ///
-    ////////////////////////////////////////////////////////////////
-    /// @dev The address of the World ID Router contract that will be used for verifying proofs
-    IWorldID internal immutable worldId;
-    // address public worldID;
-    
-    address public rewardContract;
-    string public appId;
-    string public actionId;
+  address public rewardContract;
+  string public appId;
+  string public actionId;
 
-    ////////////////////////////////////////////////////////////////
-    ///                       CONSTRUCTOR                        ///
-    ////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////
+  ///                       CONSTRUCTOR                        ///
+  ////////////////////////////////////////////////////////////////
 
-    /// @param _worldId The address of the WorldIDRouter that will verify the proofs
-    /// @param _appId The World ID App ID (from Developer Portal)
-    /// @param _actionId The World ID Action (from Developer Portal)
-    constructor(address _worldID, address _rewardContract, string memory _appId, string memory _actionId) {
-        worldID = _worldID;
-        rewardContract = _rewardContract;
-        appId = _appId;
-        actionId = _actionId;
-    }
+  /// @param _worldId The address of the WorldIDRouter that will verify the proofs
+  /// @param _appId The World ID App ID (from Developer Portal)
+  /// @param _actionId The World ID Action (from Developer Portal)
+  constructor(
+    address _worldID,
+    address _rewardContract,
+    string memory _appId,
+    string memory _actionId
+  ) {
+    worldID = _worldID;
+    rewardContract = _rewardContract;
+    appId = _appId;
+    actionId = _actionId;
+  }
 
+  struct Event {
+    uint256 id;
+    address creator;
+    string description;
+  }
 
-    struct Event {
-        uint256 id;
-        address creator;
-        string description;
-    }
+  Event[] public events;
 
-    Event[] public events;
+  ////////////////////////////////////////////////////////////////
+  ///                        FUNCTIONS                         ///
+  ////////////////////////////////////////////////////////////////
 
+  //fetch event by ID
+  function getEvent(uint256 id) public view returns (Event memory) {
+    if (id >= events.length) revert("Event does not exist");
+    return events[id];
+  }
 
-    ////////////////////////////////////////////////////////////////
-    ///                        FUNCTIONS                         ///
-    ////////////////////////////////////////////////////////////////
+  // Fetch all events
+  function getAllEvents() public view returns (Event[] memory) {
+    return events;
+  }
 
-    //fetch event by ID
-    function getEvent(uint256 id) public view returns (Event memory) {
-        if (id >= events.length) revert("Event does not exist");
-        return events[id];
-    }
-
-    // Fetch all events
-    function getAllEvents() public view returns (Event[] memory) {
-        return events;
-    }
-
-    // Function to add events for testing purposes
-    function addEventForTesting(uint256 id, string memory description, address creator) public {
-        events.push(Event({id: id, description: description, creator: creator}));
-    }
+  // Function to add events for testing purposes
+  function addEventForTesting(uint256 id, string memory description, address creator) public {
+    events.push(Event({ id: id, description: description, creator: creator }));
+  }
 }
--- test/EventManager.t.sol
+++ test/EventManager.t.sol
@@ -1,46 +1,46 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "../src/EventManager.sol";
 
 contract EventManagerTest is Test {
-    EventManager private eventManager;
+  EventManager private eventManager;
 
-    function setUp() public {
-        eventManager = new EventManager();
-    }
+  function setUp() public {
+    eventManager = new EventManager(address(1), address(2), "appId", "actionId");
+  }
 
-    function testEventManagerConstruction() public {
-        assertEq(eventManager.worldID(), address(1));
-        assertEq(eventManager.rewardContract(), address(2));
-        assertEq(eventManager.appId(), "appId");
-        assertEq(eventManager.actionId(), "actionId");
-    }
+  function testEventManagerConstruction() public {
+    assertEq(eventManager.worldID(), address(1));
+    assertEq(eventManager.rewardContract(), address(2));
+    assertEq(eventManager.appId(), "appId");
+    assertEq(eventManager.actionId(), "actionId");
+  }
 
-    function testGetEvent() public {
-        // Use the test function to add an event
-        eventManager.addEventForTesting(0, "Test Event", address(this));
+  function testGetEvent() public {
+    // Use the test function to add an event
+    eventManager.addEventForTesting(0, "Test Event", address(this));
 
-        EventManager.Event memory evt = eventManager.getEvent(0);
-        assertEq(evt.description, "Test Event");
-        assertEq(evt.creator, address(this));
-        assertEq(evt.id, 0);
-    }
+    EventManager.Event memory evt = eventManager.getEvent(0);
+    assertEq(evt.description, "Test Event");
+    assertEq(evt.creator, address(this));
+    assertEq(evt.id, 0);
+  }
 
-    function testGetNonExistentEvent() public {
-        vm.expectRevert("Event does not exist");
-        eventManager.getEvent(0);
-    }
+  function testGetNonExistentEvent() public {
+    vm.expectRevert("Event does not exist");
+    eventManager.getEvent(0);
+  }
 
-    function testGetAllEvents() public {
-        // Use the test function to add events
-        eventManager.addEventForTesting(0, "First Event", address(this));
-        eventManager.addEventForTesting(1, "Second Event", address(this));
+  function testGetAllEvents() public {
+    // Use the test function to add events
+    eventManager.addEventForTesting(0, "First Event", address(this));
+    eventManager.addEventForTesting(1, "Second Event", address(this));
 
-        EventManager.Event[] memory events = eventManager.getAllEvents();
-        assertEq(events.length, 2);
-        assertEq(events[0].description, "First Event");
-        assertEq(events[1].description, "Second Event");
-    }
+    EventManager.Event[] memory events = eventManager.getAllEvents();
+    assertEq(events.length, 2);
+    assertEq(events[0].description, "First Event");
+    assertEq(events[1].description, "Second Event");
+  }
 }
