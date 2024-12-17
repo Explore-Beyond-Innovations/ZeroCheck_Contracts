86cf62a41bfb7a1c937476c51aa34bc50661d3f3
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-12-17 19:28:46 +0000

    Format the code

--- src/interfaces/IWorldID.sol
+++ src/interfaces/IWorldID.sol
@@ -1,7 +1,12 @@
-
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
 interface IWorldID {
-  function verifyProof(uint256, address, bytes calldata) external returns (bool);
+  function verifyProof(
+    uint256 root,
+    uint256 nullifierHash,
+    uint256[8] calldata proof
+  )
+    external
+    view;
 }
